a7619d036f27f67bff1579aa9187df51915abff3
AlencarSQL <yuriclayalencar@gmail.com>
2025-10-01 10:52:24 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from verificador import verificar_par_impar
+
+def main():
+    numero = int(input("Digite um número: "))
+    resultado = verificar_par_impar(numero)
+    print(resultado)
+
+if __name__ == "__main__":
+    main()
